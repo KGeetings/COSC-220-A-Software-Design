6eeadfffebcac4fa2311665c6c097cd8ed627d28
Kenyon <kengeet56@gmail.com>
2022-08-24 13:23:50 -0500

    test build

--- BoardGame/src/boardgame/BoardGame.java
+++ BoardGame/src/boardgame/BoardGame.java
@@ -8,7 +8,7 @@ public class BoardGame {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Hello Java");  
     }
     
 }
