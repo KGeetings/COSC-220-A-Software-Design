f64617b42c4a52839429e526db3771e17c24b930
Kenyon <kengeet56@gmail.com>
2022-08-25 19:39:07 -0500

    changed driver.java to better fit requirements

--- BoardGame/src/boardgame/Driver.java
+++ BoardGame/src/boardgame/Driver.java
@@ -11,17 +11,33 @@ Then the method should prompt the user for guesses and tell the user if that gue
 public class Driver {
     
     public static void main(String[] args) {
-        Board b = new Board();
-        // print out the board to see if it is working
-        b.printBoard();
-
+        // get input from the user, using the Scanner class
         Scanner inputScanner = new Scanner(System.in);
+        // prompt the user for the dimensions of the board and the number of hot spots
+        System.out.println("Enter the dimensions of the board: ");
+        int xDim = inputScanner.nextInt();
+        int yDim = inputScanner.nextInt();
+        System.out.println("Enter the number of hot spots: ");
+        int numHotSpots = inputScanner.nextInt();
+        // create a board object by calling the appropriate constructor
+        Board gameBoard = new Board(xDim, yDim, numHotSpots);
 
-        System.out.println("Enter x and y guess: ");
-        int x = inputScanner.nextInt();
-        int y = inputScanner.nextInt();
-        System.out.println(b.guess(x,y));
-        
+        // for testing, print the game board
+        gameBoard.printBoard();
+
+        // prompt the user for guesses and tell the user if that guess is a hot spot until the user says that they don't want to guess any more
+        System.out.println("Enter your guesses (x, y): ");
+        while (inputScanner.hasNextInt()) {
+            int x = inputScanner.nextInt();
+            int y = inputScanner.nextInt();
+            if (gameBoard.guess(x, y) == "hot spot") {
+                System.out.println("hot spot");
+            } else {
+                System.out.println("cold spot");
+            }
+            System.out.println("Enter your guesses (x, y): ");
+        }
+        // close the Scanner object
         inputScanner.close();
     }
     
