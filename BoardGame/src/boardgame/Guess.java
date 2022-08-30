58aeff539887ca092431d76e55d94f6d64e004db
Kenyon <kengeet56@gmail.com>
2022-08-30 18:11:35 -0500

    initial commit of guess and player classes

---
+++ BoardGame/src/boardgame/Guess.java
@@ -0,0 +1,11 @@
+package boardgame;
+
+public class Guess {
+    /* Add a Guess class that will keep information about a single guess. 
+    The instance variables for this class should be an x and y location (or row, column), 
+    a Boolean value that indicates if the guess was successful or not, and another int that is the distance that guess is from a hotspot. 
+    The class only needs to have a single constructor which takes parameters for each of the instance variables and “accessor” methods 
+    for each of the instance variables which returns the value of that instance variable. */
+
+    
+}
---
+++ BoardGame/src/boardgame/Player.java
@@ -0,0 +1,14 @@
+package boardgame;
+
+public class Player {
+    /* Add a Player class that will keep information about a single player. 
+    The class should have instance variables for the name of the player, 
+    the number of hotspots they guessed correctly and a list of the guesses they have made (you can use an array, or Java’s ArrayList or LinkedList classes). 
+    The class should have a single constructor method to initialize the instance variables. 
+    Only the name needs to passed as a parameter. 
+    Initialize the number of hot spots guessed and the list of guesses made to an appropriate value. 
+    It should have accessor methods to get the players name and the number of hot spots guessed. 
+    It should also have “mutator” methods to add 1 to the number of hot spots guessed and to add a guess to the list of guesses. */
+
+    
+}
