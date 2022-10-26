4dd47d6899de05119746d9dc8d023206c4fd354b
Kenyon <kengeet56@gmail.com>
2022-10-26 10:05:34 -0500

    first commit JUnitQuiz

---
+++ JunitQuiz/test/CalcEngineTest.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author FYFES
+ */
+public class CalcEngineTest {
+    CalcEngine calculator;
+    
+    public CalcEngineTest() {
+    }
+    
+    @Before
+    public void setUp() {
+        calculator = new CalcEngine();
+    }
+
+    @Test
+    public void testMethod1() {
+    }
+
+   
+    
+}
