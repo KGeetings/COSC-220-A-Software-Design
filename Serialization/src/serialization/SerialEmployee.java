f10967146a3b07e63799368406fbf88fcd50e42b
Kenyon <kengeet56@gmail.com>
2022-11-02 10:23:16 -0500

    new fun stuff

--- Serialization/src/serialization/SerialEmployee.java
+++ Serialization/src/serialization/SerialEmployee.java
@@ -1,6 +1,7 @@
 package serialization;
 
 import java.io.*;
+import java.util.Date;
 
 public class SerialEmployee {
     // create a new employee
@@ -9,12 +10,12 @@ public class SerialEmployee {
 
     public static void main(String[] args) {
         // create a new employee
-        Employee harry = new Employee("Harry Hacker", 50000, 1989, 10, 1);
+        Employee harry = new Employee("Harry Hacker", 50000, 2001, 6, 22);
 
         // save the employee to a file
         try {
             // use a file output stream to write to a file
-            FileOutputStream fileOut = new FileOutputStream("/Serialization/employee.dat");
+            FileOutputStream fileOut = new FileOutputStream("./Serialization/employee.dat");
             // use an object output stream to write objects to a stream
             ObjectOutputStream out = new ObjectOutputStream(fileOut);
             // write the object to the stream
@@ -29,7 +30,7 @@ public class SerialEmployee {
         // read the employee from a file
         try {
             // use a file input stream to read from a file
-            FileInputStream fileIn = new FileInputStream("/Serialization/employee.dat");
+            FileInputStream fileIn = new FileInputStream("./Serialization/employee.dat");
             // use an object input stream to read objects from a stream
             ObjectInputStream in = new ObjectInputStream(fileIn);
             // read the object from the stream
@@ -39,6 +40,22 @@ public class SerialEmployee {
             fileIn.close();
             // print the object
             System.out.println(newHarry);
+            // print details of the object
+            System.out.println("Employee name: " + newHarry.getName());
+            System.out.println("Employee salary: " + newHarry.getSalary());
+            System.out.println("Employee hire day: " + newHarry.getHireDay());
+
+            // Calculate how long the employee has been working
+            // get the current date
+            Date today = new Date();
+            // get the hire day
+            Date hireDay = newHarry.getHireDay();
+            // calculate the difference in milliseconds
+            long difference = today.getTime() - hireDay.getTime();
+            // convert the difference to years
+            long years = difference / 1000 / 60 / 60 / 24 / 365;
+            // print the number of years
+            System.out.println("Employee has worked for " + years + " years");
         } catch (IOException i) {
             i.printStackTrace();
         } catch (ClassNotFoundException c) {
