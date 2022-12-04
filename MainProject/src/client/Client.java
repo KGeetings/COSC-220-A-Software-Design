c298fc293a97776ceb07387b676ae41bec5e6db5
Kenyon <kengeet56@gmail.com>
2022-12-04 13:26:07 -0600

    downgrading project to work on Central's old machines

--- MainProject/src/client/Client.java
+++ MainProject/src/client/Client.java
@@ -18,7 +18,7 @@ public class Client {
      */
 
     //keep server's IP address
-    public static String serverIP = "192.168.1.45";
+    public static String serverIP = "10.21.60.83";
 
     //keep track of the user's username
     public static String username;
